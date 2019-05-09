d94785dcdd63e2391a8b22ee663ad22c4dbd0272
Edgar F <eflores.ef@gmail.com>
2019-05-08 23:06:25 -0700

    post method

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 // implement your API here
-// require the express npm module, needs to be added to the project using "yarn add" or "npm install"
+
 const express = require('express');
-
-// creates an express application using the express module
+const db = require('./data/db');
+const port = 7000;
 const server = express();
+server.use(express.json());
 
-// configures our server to execute a function for every GET request to "/"
-// the second argument passed to the .get() method is the "Route Handler Function"
-// the route handler function will run on every GET request to "/"
-server.get('/', (req, res) => {
-  // express will pass the request and response objects to this function
-  // the .send() on the response object can be used to send a response to the client
-  res.send('Hello World');
-});
+server.post('/api/users', (req, res) => {
+   const newUser = req.body;
 
-server.get('/hobbits', (req, res) => {
-   // route handler code here
-   const hobbits = [
-      {
-        id: 1,
-        name: 'Samwise Gamgee',
-      },
-      {
-        id: 2,
-        name: 'Frodo Baggins',
-      },
-    ];
-    
-   res.status(200).json(hobbits);
- });
+   db.insert(newUser)
+   .then(addedUser => {
+      res.status(201).json(addedUser);
+   })
+   .catch(({ code, message }) => {
+      res.status(code).json({ err: message })
+   })
+})
 
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
-// the callback function passed as the second argument will run once when the server starts
-server.listen(8000, () => console.log('API running on port 8000'));
+server.listen(port, () => {
+   console.log(`Listening on port ${port}`)
+})
